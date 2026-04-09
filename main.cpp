8000167d4264e3cc2e6a8e3428b734c6473f21cd
aleksej yanovskiiij <kvinsi2018@gmail.com>
2026-04-09 14:15:47 +0500

    modified main to use the new functions

--- main.cpp
+++ main.cpp
@@ -32,14 +32,13 @@ void save_to_file(flight* arr, int size, const char* filename) {
     file.close();
 }
 
-void load_from_file(flight* arr, int& size, const char* filename) {
+void load_from_file(flight* arr, int size, const char* filename) {
     std::ifstream file(filename, std::ios::binary);
     if (!file) {
         std::cerr << "Файл не найден, создан новый массив" << '\n';
         return;
     }
     file.read(reinterpret_cast<char*>(arr), FLIGHTS_COUNT*sizeof(flight));
-    size = FLIGHTS_COUNT;
     file.close();
 }
 
@@ -158,28 +157,41 @@ void wrapper_print(flight* arr, int len, const char* filter_name) {
 
 int main() {
     const int CHARTERS_TO_PRINT = 5;
-    flight flights[FLIGHTS_COUNT] = {
-        {"москваград",    "08:00", "10:30", 150, 5000, TRANSIT },
-        {"санктград",     "12:00", "14:00", 120, 4500, CHARTER },
-        {"новоград",      "23:30", "01:00", 90,  3000, CHARTER },
-        {"киевград",      "09:00", "11:00", 120, 4000, TRANSIT },
-        {"минскград",     "22:00", "23:30", 90,  3500, CHARTER },
-        {"лондонград",    "14:00", "17:00", 180, 8000, COUPLING},
-        {"парижград",     "20:00", "23:30", 210, 7500, CHARTER },
-        {"берлинград",    "03:00", "05:00", 120, 4200, TRANSIT },
-        {"рим",           "10:00", "12:30", 150, 5500, CHARTER },
-        {"мадридград",    "01:00", "03:30", 150, 4800, CHARTER },
-        {"ослоград",      "16:00", "18:00", 120, 3800, COUPLING},
-        {"стокгольм",     "04:00", "06:30", 150, 5200, TRANSIT },
-        {"варшавград",    "13:00", "15:00", 120, 4100, CHARTER },
-        {"прагаград",     "21:00", "23:00", 120, 4300, COUPLING},
-        {"будапештград",  "02:00", "04:00", 120, 3900, TRANSIT },
-        {"венаград",      "11:00", "13:30", 150, 4600, CHARTER },
-        {"амстердамград", "23:00", "01:30", 150, 6200, CHARTER },
-        {"брюссельград",  "05:00", "07:00", 120, 4400, COUPLING},
-        {"лиссабонград",  "19:00", "22:00", 180, 5800, TRANSIT },
-        {"афины",         "00:30", "02:30", 120, 4900, CHARTER },
-    };
+    flight flights[FLIGHTS_COUNT];
+    load_from_file(flights, FLIGHTS_COUNT, "flights.txt");
+
+    bool file_exists = true;
+    std::ifstream test("flights.txt", std::ios::binary);
+    if (!test) {
+        file_exists = false;
+        flight default_flights[20] = {
+            {"москваград",    "08:00", "10:30", 150, 5000, TRANSIT},
+            {"санктград",     "12:00", "14:00", 120, 4500, CHARTER},
+            {"новоград",      "23:30", "01:00", 90, 3000,  CHARTER},
+            {"киевград",      "09:00", "11:00", 120, 4000, TRANSIT},
+            {"минскград",     "22:00", "23:30", 90, 3500,  CHARTER},
+            {"лондонград",    "14:00", "17:00", 180, 8000, COUPLING},
+            {"парижград",     "20:00", "23:30", 210, 7500, CHARTER},
+            {"берлинград",    "03:00", "05:00", 120, 4200, TRANSIT},
+            {"рим",           "10:00", "12:30", 150, 5500, CHARTER},
+            {"мадридград",    "01:00", "03:30", 150, 4800, CHARTER},
+            {"ослоград",      "16:00", "18:00", 120, 3800, COUPLING},
+            {"стокгольмград", "04:00", "06:30", 150, 5200, TRANSIT},
+            {"варшавград",    "13:00", "15:00", 120, 4100, CHARTER},
+            {"прагаград",     "21:00", "23:00", 120, 4300, COUPLING},
+            {"будапештград",  "02:00", "04:00", 120, 3900, TRANSIT},
+            {"венаград",      "11:00", "13:30", 150, 4600, CHARTER},
+            {"амстердамград", "23:00", "01:30", 150, 6200, CHARTER},
+            {"брюссельград",  "05:00", "07:00", 120, 4400, COUPLING},
+            {"лиссабонград",  "19:00", "22:00", 180, 5800, TRANSIT},
+            {"афины",         "00:30", "02:30", 120, 4900, CHARTER}
+        };
+        for (int i = 0; i < 20; i++) {
+            flights[i] = default_flights[i];
+        }
+        save_to_file(flights, 20, "flights.txt");
+    }
+    test.close();
 
     flight transit_flights[FLIGHTS_COUNT];
     int transit_count;
@@ -199,6 +211,7 @@ int main() {
     print_night_charter(flights, FLIGHTS_COUNT, CHARTERS_TO_PRINT);
 
     update_flight(flights, FLIGHTS_COUNT, "москваград");
+    save_to_file(flights, FLIGHTS_COUNT, "flights.txt");
 
     flight cheap_connection[FLIGHTS_COUNT];
     int cheap_count;
